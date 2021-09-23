6d7a5495eb5d32c0298f2a8301b927f5ee36145c
wiñay wayna <wilsonfmamani@yahoo.com>
2021-09-22 21:24:32 -0500

    Update main.js

--- curso/main.js
+++ curso/main.js
@@ -1,9 +1,37 @@
-let sebastian = {
-    name: "sebastian",
-    age: 33,
-    getName: function () {
-        console.log(this)
-    }
-}
+// let sebastian = {
+//     name: "sebastian",
+//     age: 33,
+//     getName: function () {
+//         console.log(this)
+//     }
+// }
 
-console.log(sebastian.name, sebastian.age)
+// console.log(sebastian.name, sebastian.age)
+
+
+//arrays
+
+// let array = [0, 1, 2, 33, 24, 15]
+// console.log(array[array.length-1])
+
+
+
+// let array = ["jose","maria","jesus"];
+
+let asrray = [
+    {
+        name:"jose",
+        age: 12
+    },
+    {
+        name:"maria",
+        age: 23
+    },
+    {
+        name: "jesus",
+        age: 23
+    }
+];
+
+
+console.log(asrray)
